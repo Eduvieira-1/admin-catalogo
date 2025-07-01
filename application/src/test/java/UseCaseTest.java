8168deb820e7a2e7c73f9795c00993520b6431c7
Eduvieira-1 <edu.vieira1@icloud.com>
2025-06-30 22:22:59 -0300

    inital tests

---
+++ application/src/test/java/UseCaseTest.java
@@ -0,0 +1,15 @@
+import com.admin.catalogo.application.UseCase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UseCaseTest {
+
+    @Test
+    public void testCreateUseCase(){
+        Assertions.assertNotNull(new UseCase());
+        Assertions.assertNotNull(new UseCase().execute());
+    }
+
+}
+
+
--- src/main/java/com/admin/catalogo/Main.java
+++
@@ -1,7 +0,0 @@
-package com.admin.catalogo;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
