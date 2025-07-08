ef5c2dad1fc0d5939116dbd64baf84d796cf7939
Eduvieira-1 <edu.vieira1@icloud.com>
2025-07-08 07:02:59 -0300

    use cases and tdd create category

--- application/src/main/java/com/admin/catalogo/application/NullaryUseCase.java
+++ application/src/main/java/com/admin/catalogo/application/NullaryUseCase.java
@@ -2,6 +2,6 @@ package com.admin.catalogo.application;
 
 public abstract class NullaryUseCase<OUT> {
 
-    public abstract OUT execute(OUT anIn);
+    public abstract OUT   execute(OUT anIn);
 }
 
